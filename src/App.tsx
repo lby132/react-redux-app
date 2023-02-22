3b27febb4b89b480079ec98ea5305545de126a1c
lby132 <quddbscjswow@naver.com>
2023-02-22 23:19:34 +0900

    redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
-import React, {useState} from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
-import {useDispatch, useSelector} from "react-redux";
-import {RootState} from "./reducers";
+import { useDispatch, useSelector } from 'react-redux';
+import { RootState } from './reducers';
+import axios from "axios";
+import {fetchPosts} from "./action/posts";
 
 type Props = {
     value: any;
@@ -9,11 +11,23 @@ type Props = {
     onDecrement: () => void;
 }
 
+interface Post {
+    userId: number;
+    id: number;
+    title: string;
+}
+
 function App({value, onIncrement, onDecrement}: Props) {
     const dispatch = useDispatch();
     const counter = useSelector((state: RootState) => state.counter);
     const todos: string[] = useSelector((state: RootState) => state.todos);
+    const posts:Post[] = useSelector((state: RootState) => state.posts);
     const [todoValue, setTodoValue] = useState("");
+
+    useEffect(() => {
+        dispatch(fetchPosts())
+    }, [dispatch]);
+
     const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         setTodoValue(e.target.value);
     }
@@ -41,6 +55,10 @@ function App({value, onIncrement, onDecrement}: Props) {
                 <input type="text" value={todoValue} onChange={handleChange}/>
                 <input type="submit"/>
             </form>
+
+            <ul>
+                {posts.map((post, index) => <li key={index}>{post.title}</li>)}
+            </ul>
         </div>
     );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -3,33 +3,36 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {createStore} from "redux";
-import rootReducer from "./reducers";
-import {Provider} from "react-redux";
+import { applyMiddleware, createStore } from 'redux';
+import rootReducer from './reducers';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
 
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
 );
 
-const store = createStore(rootReducer);
+const loggerMiddleware = (store: any) => (next: any) => (action: any) => {
+    console.log("store", store);
+    console.log("action", action);
+    next(action);
+}
 
-store.dispatch({
-    type: "ADD_TODO",
-    text: "USE REDUX"
-})
-console.log('store.getState', store.getState());
+const middleware = applyMiddleware(thunk, loggerMiddleware);
+
+const store = createStore(rootReducer, middleware);
+
 const render = () => root.render(
     <React.StrictMode>
-        <Provider store={store}>
+        <Provider store={store} >
             <App
                 value={store.getState()}
-                onIncrement={() => store.dispatch({type: 'INCREMENT'})}
-                onDecrement={() => store.dispatch({type: 'DECREMENT'})}
+                onIncrement={() => store.dispatch({ type: "INCREMENT" })}
+                onDecrement={() => store.dispatch({ type: "DECREMENT" })}
             />
         </Provider>
-    </React.StrictMode>
+    </React.StrictMode >
 );
-
 render();
 
 store.subscribe(render);
